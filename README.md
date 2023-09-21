1978cd8c20267d315bf71339afbca5a2d4b6aeb6
koushalm24 <skoushal4301@gmail.com>
2023-09-21 17:26:51 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Weather App using React
+
+![GitHub](https://img.shields.io/github/license/yourusername/weather-app-react)
+![GitHub last commit](https://img.shields.io/github/last-commit/koushalm24/weather-app-react)
+
+## Project Overview
+
+This is a simple Weather App built with React that provides current weather information using the OpenWeather API. It allows users to check the temperature, weather conditions (e.g., cloud, clear, drizzle), wind speed, and humidity for a specific location.
+
+## Features
+
+- Display current temperature in Celsius for a given location.
+- Show weather conditions (e.g., cloud, clear, drizzle) for the location.
+- Provide wind speed information.
+- Display humidity percentage.
+
+## Technologies Used
+
+- React: A JavaScript library for building user interfaces.
+- OpenWeather API: An open-source weather data provider.
+- HTML/CSS: For structuring and styling the application.
+- JavaScript: For application logic and API interaction.
+
+## Getting Started
+
+To get started with this project, follow these steps:
+
+1. Clone the repository to your local machine:
+
+
+git clone https://github.com/yourusername/weather-app-react.git 
+
+2. In the project directory, you can run:
+
+`npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+
---
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react'
+import './WeatherApp.css'
+
+import clear_icon from '../Assets/clear.png'
+import cloud_icon from '../Assets/cloud.png'
+import drizzle_icon from '../Assets/drizzle.png'
+import humidity_icon from '../Assets/humidity.png'
+import rain_icon from '../Assets/rain.png'
+import search_icon from '../Assets/search.png'
+import snow_icon from '../Assets/snow.png'
+import wind_icon from '../Assets/wind.png'
+import error_icon from '../Assets/404.png'
+
+function WeatherApp() {
+  let api_key = "ced627980493a6e1ccaf75c7a513610d";
+
+  const [wicon, setWicon] = useState(clear_icon);
+  const [visible, setVisible] = useState(true);
+
+  const handleKeyDown = event => {
+    if (event.key === 'Enter') {
+      search();
+    }
+  };
+  const search = async () => {
+    const element = document.getElementsByClassName('cityInput');
+
+    if (element[0].value === "") {
+      alert("Please Enter A City To search");
+      return 0;
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+
+
+    let response = await fetch(url);
+    let data = await response.json();
+
+    const humidity = document.getElementsByClassName("humidity-percentage");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temperature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
+    const condition = document.getElementsByClassName("condition");
+
+    if (data.cod == 404) {
+      setVisible(false);
+
+      location[0].innerHTML = "City Not Found !!";
+      setWicon(error_icon);
+
+    }
+    else {
+
+      humidity[0].innerHTML = data.main.humidity + "%";
+      wind[0].innerHTML = data.wind.speed + "km/h";
+      temperature[0].innerHTML = Math.floor(data.main.temp) + "°C";
+      location[0].innerHTML = data.name;
+      condition[0].innerHTML = data.weather[0].main;
+
+      if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
+        setWicon(clear_icon);
+      }
+      else if (data.weather[0].icon === "02d" || data.weather[0].icon === "02n") {
+        setWicon(cloud_icon);
+      }
+      else if (data.weather[0].icon === "03d" || data.weather[0].icon === "03n") {
+        setWicon(cloud_icon);
+      }
+      else if (data.weather[0].icon === "04d" || data.weather[0].icon === "04n") {
+        setWicon(cloud_icon);
+      }
+      else if (data.weather[0].icon === "09d" || data.weather[0].icon === "09n") {
+        setWicon(drizzle_icon);
+      }
+      else if (data.weather[0].icon === "10d" || data.weather[0].icon === "10n") {
+        setWicon(rain_icon);
+      }
+      else if (data.weather[0].icon === "13d" || data.weather[0].icon === "13n") {
+        setWicon(snow_icon);
+      }
+      else {
+        setWicon(clear_icon);
+      }
+    }
+
+
+  }
+
+  return (
+    <div className="container" >
+  
+      <div className="top-bar">
+        <input type="text" className="cityInput" placeholder="Search" onKeyDown={handleKeyDown} />
+        <div className="search-icon" onClick={() => { search() }}>
+          <img src={search_icon} alt="" />
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={wicon} alt="" />
+      </div>
+      <p className="condition"></p>
+      <div className={`weather-temp ${visible?'':'hidden'}`}>Temp in °C</div>
+      <div className="weather-location">Enter A Location</div>
+      <div className="data-container">
+
+        <div className={`element ${visible?'':'hidden'}`}>
+          <img src={humidity_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage"></div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+
+        <div className={`element ${visible?'':'hidden'}`}>
+          <img src={wind_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="wind-rate"></div>
+            <div className="text">Wind Speed</div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default WeatherApp
